97c0f4259235b29ea6a52f2c1a323e29513ff633
AdrianSP <adrianserrano15@gmail.com>
2017-11-10 20:47:51 -0500

    Add files via upload

--- client/src/index.js
+++ client/src/index.js
@@ -1,24 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import './index.css';
-import App from './App';
-import AddItem from './components/AddItem';
-import IndexBook from './components/IndexBook';
-import BookDetails from './components/BookDetails';
-import Carrousel from './components/Carrousel';
-import Header from './components/Header';
 
+import App from './App';
+import AddUser from './components/AddUser';
+import loginPage from './components/Login';
+import Profile from './components/ProfilePage';
 
 
 ReactDOM.render(
-    <Router>
-        <div>
-            <Route exact path='/' component={App} />
-            <Route path='/admin/add-item' component={AddItem} />
-            <Route path='/admin/items-books' component={IndexBook} />
-            <Route path='/books' component={BookDetails} />
-        </div>
-    </Router>,
-    document.getElementById('root')
+  <Router>
+      <div>
+        <Route exact path='/' component={App} />
+        <Route path='/add-user' component={AddUser} />
+        <Route path='/login' component={loginPage} />
+        <Route path='/profile/:id' component={Profile} />
+      </div>
+  </Router>,
+  document.getElementById('root')
 );
