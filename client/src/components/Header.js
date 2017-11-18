4662aa43c6f3352e07389e6cc4ca58abf87f68c3
Dylan <dylan.tadros@gmail.com>
2017-11-17 22:14:59 -0800

    Adds bootstrap and styles header for consistency

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,16 +1,33 @@
-import React, { Component } from 'react';
-import '../App.css';
+import React, { Component } from "react";
+import { Navbar, Nav, NavItem, Glyphicon } from "react-bootstrap";
 
-class AppHeader extends Component {
+import "../App.css";
+
+class AppHeader extends React.Component {
   render() {
     return (
-          <div className = "container">
-              <div className = "row">
-                  <div className ="col-md-12 text-center App-header">
-                      Welcome to GeekBook
-                  </div>
-              </div>
-          </div>
+      <Navbar fixedTop>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="/">Welcome to Geekstore</a>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav pullRight>
+            <NavItem eventKey={1} href="#">
+              Home
+            </NavItem>
+            <NavItem eventKey={2} href="#">
+              Profile
+            </NavItem>
+            <NavItem eventKey={3} href="#">
+              <Glyphicon glyph="shopping-cart" />
+              {" Cart"}
+            </NavItem>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     );
   }
 }
